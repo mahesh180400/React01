467834a9331bbe29604ce1caac30c11f0656e52d
mahesh180400 <149650440+mahesh180400@users.noreply.github.com>
2024-01-29 11:03:57 +0530

    Reastraurent 1

---
+++ src/components/Layout/HeaderCartButton.js
@@ -0,0 +1,10 @@
+import CartIcon from "../Cart/CartIcon";
+import Classes from './HeaderCartButton.module.css';
+const HeaderCartButton=props=>{
+    return <button className={Classes.button}>
+        <span className={Classes.icon}><CartIcon /></span>
+        <span>Your Cart</span>
+        <span className={Classes.badge}>3</span>
+    </button>
+};
+export default HeaderCartButton;
