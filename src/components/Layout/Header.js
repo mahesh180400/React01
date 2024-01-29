9c2f0455220bf5bd1b940e8c3afed7e9fdb51043
mahesh180400 <149650440+mahesh180400@users.noreply.github.com>
2024-01-29 18:03:00 +0530

    Reastraurent 2

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,4 +1,4 @@
-import {Fragment} from 'react'
+import React,{Fragment} from 'react'
 import classes from './Header.module.css';
 import HeaderCartButton from './HeaderCartButton';
 const Header=(props)=>{
@@ -7,7 +7,7 @@ const Header=(props)=>{
     <Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-          <HeaderCartButton />
+            <HeaderCartButton/>
          </header>
         <div className={classes['main-image']}>
             <img src='https://github.com/academind/react-complete-guide-code/blob/11-practice-food-order-app/extra-files/meals.jpg?raw=true' alt='A table Full of Delicious Food'/>
