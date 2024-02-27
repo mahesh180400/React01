c79c9dbe323ed9f266483bb359e16b96bb86e4a9
mahesh180400 <149650440+mahesh180400@users.noreply.github.com>
2024-02-27 15:27:03 +0530

    noloading

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 import { useState } from 'react';
 function App() {
-  const [movies,setmovies]=useState([])
- function fetchMoviesHandler(){
-  fetch('https://swapi.dev/api/films/').then((response)=>{
-   return  response.json();
-  }).then((data)=>{
-    const transformedMovies=data.results.map(moviedata=>{
+  const [movies,setmovies]=useState([]);
+  const [isloading,setisloading]=useState(false)
+ 
+  async function fetchMoviesHandler(){
+  const response= await fetch('https://swapi.dev/api/films/')
+  const data=await response.json()
+  setisloading(true)
+ const transformedMovies=data.results.map(moviedata=>{
       return {
         id:moviedata.episode_id,
         title:moviedata.title,
@@ -18,7 +20,8 @@ function App() {
       }
     })
     setmovies(transformedMovies);
-  })
+    setisloading(false)
+  
  }
 
   return (
@@ -27,7 +30,9 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+       {!isloading&& movies.length>0 &&<MoviesList movies={movies} />}
+       {isloading&&<h2>Loading....</h2>}
+       {movies.length===0 && <p>Found No movies</p>}
       </section>
     </React.Fragment>
   );
