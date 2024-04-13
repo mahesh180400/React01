d82a7cd4b41e5e4656ab7674c205e82ac43474fd
mahesh180400 <149650440+mahesh180400@users.noreply.github.com>
2024-04-13 16:30:58 +0530

    react redux

--- src/Login/Greeting.js
+++ src/Login/Greeting.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
 const Greeting=()=>{
+const [changeText,setchangeText]=useState(false)
+const changeTextHandler=()=>{
+    setchangeText(true);
+}
     return (
         <div>
         <h2>Hello Sir/Mam</h2>
-        <p>It's good to see you!</p>
+        {!changeText && <p>It's good to see you!</p>}
+        {changeText && <p>Done</p>}
+        <button onClick={changeTextHandler}>Change Text!</button>
         </div>
     )
 };
--- src/Login/Greeting.test.js
+++ src/Login/Greeting.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Greeting from './Greeting';
+
 test('rendering', () => {
-    render(<Greeting></Greeting>)
+    render(<Greeting />);
     const helloText = screen.getByText("Hello Sir/Mam");
     expect(helloText).toBeInTheDocument();
-  });
-  
+});
+
+test('renders "good to see you" if button was NOT clicked', () => {
+    render(<Greeting />);
+    const outputElement = screen.getByText('good to see you', { exact: false });
+    expect(outputElement).toBeInTheDocument();
+});
+
+test('renders "Done" if the button was clicked', async () => {
+    render(<Greeting />);
+    const buttonElement = screen.getByRole('button');
+    userEvent.click(buttonElement);
+
+    await waitFor(() => {
+        const outputElement = screen.getByText('Done');
+        expect(outputElement).toBeInTheDocument();
+    });
+});
