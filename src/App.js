7561ca1a0dc3f179e301b2dbdf8b34b4fdd827c8
mahesh180400 <149650440+mahesh180400@users.noreply.github.com>
2024-04-30 12:09:01 +0530

    3 task

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import SignupForm from "./Starting/SignupForm";
-import BasicExample from "./Starting/ColorSchemesExample";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signup from './Login/Signup';
+import ProfileEdit from './Login/ProfileEdit';
+import Main from './Main/Main';
+import Forgot from './Login/Forgot';
 function App() {
   return (
- <>
-  <BasicExample></BasicExample>
-  <SignupForm></SignupForm> 
-  </>
+    <>
+    
+      <Router>
+        <Routes>
+          <Route path="*" element={<Signup></Signup>}></Route>
+          <Route path="" element={<Signup />} />
+          <Route path="/profile_edit" element={<ProfileEdit></ProfileEdit>} />
+          <Route path="/main" element={<Main></Main>}/>
+          <Route path="/forgot_password" element={<Forgot></Forgot>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
+// index.js or App.js
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './Store/store';
+import { Provider } from 'react-redux';
+const rootElement = document.getElementById('root');
 
+const createRoot = () => {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+        </Provider>
+    </React.StrictMode>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// For React 18
+if (rootElement.createReactRoot) {
+  rootElement.createReactRoot();
+  createRoot();
+} else {
+  createRoot();
+}
